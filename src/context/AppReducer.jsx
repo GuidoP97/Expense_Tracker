bc8f7ea604c0a81eb1c37234ec9feb993ad40cb3
GuidoP97 <guidopodesta99@gmail.com>
2021-04-13 15:40:41 -0300

    i incorporated function that add transactions

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,11 +1,18 @@
-export default (state, action) => {
+const fn = (state, action) => {
   switch(action.type){
     case 'DELETE_TRANSACTION':
       return {
         ...state,
         transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
       };
+    case 'ADD_TRANSACTION':
+      return {
+        ...state,
+        transactions:[action.payload, ...state.transactions],
+      }
     default:
       return state;
   }
 }
+
+export default fn
